57d750b6c18a5b13201efac624685f8cb687984b
7220 <kumasaki@naver.com>
2023-05-20 16:53:42 +0900

    detail add

--- polygon.js
+++ polygon.js
@@ -1,38 +1,47 @@
-const PI2 = Math.PI * 2
+const PI2 = Math.PI * 2;
 
 export class Polygon {
   constructor(x, y, radius, sides) {
-    this.x = x
-    this.y = y
-    this.radius = radius
-    this.sides = sides
-    this.rotate = 0 
+    this.x = x;
+    this.y = y;
+    this.radius = radius;
+    this.sides = sides;
+    this.rotate = 0;
   }
 
   animate(ctx, moveX) {
-    ctx.save()
-    ctx.fillStyle = '#5af'
-    ctx.beginPath()
+    ctx.save();
+    ctx.fillStyle = "#5af";
+    // ctx.beginPath()
 
-    const angle = PI2 /this.sides
+    const angle = PI2 / this.sides;
+    const angle2 = PI2 / 4;
 
-    ctx.translate(this.x, this.y)
+    ctx.translate(this.x, this.y);
 
-    this.rotate -= moveX * 0.008
-    ctx.rotate(this.rotate)
+    this.rotate -= moveX * 0.002;
+    ctx.rotate(this.rotate);
 
     for (let i = 0; i < this.sides; i++) {
-      const x = this.radius * Math.cos(angle * i)
-      const y = this.radius * Math.sin(angle * i)
+      const x = this.radius * Math.cos(angle * i);
+      const y = this.radius * Math.sin(angle * i);
 
-      i == 0 ? ctx.moveTo(x, y) : ctx.lineTo(x, y)
-      // ctx.beginPath()
-      // ctx.arc(x, y, 100, 0, PI2, false)
-      // ctx.fill()
+      ctx.save();
+      ctx.translate(x, y);
+      ctx.rotate((((360 / this.sides) * i + 45) * Math.PI) / 180);
+      ctx.beginPath();
+      for (let j = 0; j < 4; j++) {
+        const x2 = 150 * Math.cos(angle2 * j);
+        const y2 = 150 * Math.sin(angle2 * j);
+        j == 0 ? ctx.moveTo(x2, y2) : ctx.lineTo(x2, y2);
+      }
+      ctx.fill();
+      ctx.closePath();
+      ctx.restore();
     }
 
-    ctx.fill()
-    ctx.closePath()
-    ctx.restore()
+    // ctx.fill()
+    // ctx.closePath()
+    ctx.restore();
   }
 }
